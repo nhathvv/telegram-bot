01589aa6e3c569c6a24390bae5ba35e885230c72
nhathv <nhathv.21it@gmail.com>
2024-12-11 18:19:48 +0700

    Migrate bot functionality to index.js and update start script in package.json

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
 const TelegramBot = require('node-telegram-bot-api');
 const schedule = require('node-schedule');
 require('dotenv').config();
@@ -72,5 +76,8 @@ bot.on('message', (msg) => {
     }
   }
 });
+console.log('Bot is running...');
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
-console.log('Bot Telegram đang chạy...');
